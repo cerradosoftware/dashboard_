a007e25c43c89e1654a956a8bcce065a2ae13f5f
Fernando Ridam <30510101+FernandoRidam@users.noreply.github.com>
2022-05-07 12:59:37 -0300

    Dados mocados

--- src/routes.js
+++ src/routes.js
@@ -18,65 +18,60 @@ routes.get("/api/average/energy/power", ( req, res ) => {
 });
 
 routes.get('/api/daily/energy/performance', ( req, res ) => {
-  return res.json([{"date":"2016-12-21","performance":50.5},{"date":"2016-12-22","performance":63.099999999999994},{"date":"2016-12-23","performance":44.2},{"date":"2016-12-24","performance":58.3},{"date":"2016-12-25","performance":65},{"date":"2016-12-26","performance":53.2},{"date":"2016-12-27","performance":76.6},{"date":"2016-12-28","performance":44.6},{"date":"2016-12-29","performance":58.099999999999994},{"date":"2016-12-30","performance":35.9},{"date":"2016-12-31","performance":72.4}]);
+  return res.json([{"date":"2016-12-21","performance":50.5},{"date":"2016-12-22","performance":63.099999999999994},{"date":"2016-12-23","performance":44.2},{"date":"2016-12-24","performance":58.3},{"date":"2016-12-25","performance":65},{"date":"2016-12-26","performance":53.2},{"date":"2016-12-27","performance":76.6},{"date":"2016-12-28","performance":44.6},{"date":"2016-12-29","performance":58.099999999999994},{"date":"2016-12-30","performance":35.9}]);
 });
 
 routes.get('/api/daily/energy/power', ( req, res ) => {
   return res.json([
     {
       date: "2016-12-21",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 20.05,
+      availablePower: 25.141767926859963,
     },
     {
       date: "2016-12-22",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 19.745,
+      availablePower: 25.109418121043287,
     },
     {
       date: "2016-12-23",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: null,
+      availablePower: null,
     },
     {
       date: "2016-12-24",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 24.9,
+      availablePower: 25.02,
     },
     {
       date: "2016-12-25",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 23.2,
+      availablePower: 23.4,
     },
     {
       date: "2016-12-26",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 8.9,
+      availablePower: 25.02,
     },
     {
       date: "2016-12-27",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 17.38,
+      availablePower: 25.175921453401592,
     },
     {
       date: "2016-12-28",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 21.48086956521739,
+      availablePower: 25.610399091768844,
     },
     {
       date: "2016-12-29",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 20.505416666666665,
+      availablePower: 25.357242750286165,
     },
     {
       date: "2016-12-30",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
-    },
-    {
-      date: "2016-12-31",
-      generatedPower: generateValue(),
-      availablePower: generateValue(),
+      generatedPower: 16.28708333333333,
+      availablePower: 25.24006400232699,
     },
   ]);
 });
@@ -85,49 +80,45 @@ routes.get('/api/daily/inflow', ( req, res ) => {
 
   return res.json([
     {
-      date: "2016-12-30",
-      inflow: generateValue(),
+    "date": "2016-12-30",
+    "inflow": 312.9964869294174
     },
     {
-      date: "2016-12-27",
-      inflow: generateValue(),
+    "date": "2016-12-27",
+    "inflow": 321.9182515911149
     },
     {
-      date: "2016-12-24",
-      inflow: generateValue(),
+    "date": "2016-12-24",
+    "inflow": null
     },
     {
-      date: "2016-12-26",
-      inflow: generateValue(),
+    "date": "2016-12-26",
+    "inflow": 338.407176703862
     },
     {
-      date: "2016-12-21",
-      inflow: generateValue(),
+    "date": "2016-12-21",
+    "inflow": 337.6060867962611
     },
     {
-      date: "2016-12-23",
-      inflow: generateValue(),
+    "date": "2016-12-23",
+    "inflow": 351.68691377271693
     },
     {
-      date: "2016-12-25",
-      inflow: generateValue(),
+    "date": "2016-12-25",
+    "inflow": 366.21229440936395
     },
     {
-      date: "2016-12-28",
-      inflow: generateValue(),
+    "date": "2016-12-28",
+    "inflow": 363.08127243747964
     },
     {
-      date: "2016-12-31",
-      inflow: generateValue(),
+    "date": "2016-12-31",
+    "inflow": 300.15587047042607
     },
     {
-      date: "2016-12-22",
-      inflow: generateValue(),
-    },
-    {
-      date: "2016-12-29",
-      inflow: generateValue(),
-    },
+    "date": "2016-12-22",
+    "inflow": 340.6381679010164
+    }
   ]);
 });
 
