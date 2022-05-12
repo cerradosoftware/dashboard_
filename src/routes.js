e34dd17756c54746a7f8a22d79a643bcea14bc2e
Fernando Ridam <30510101+FernandoRidam@users.noreply.github.com>
2022-05-12 17:42:49 -0300

    Valores randômicos

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 const routes = Router();
 
-routes.get('/api/average/energy/loss', ( req, res ) => {
+routes.get("/api/average/energy/loss", (req, res) => {
   return res.json({
     performance: generateValue(),
-    loss: generateValue()
+    loss: generateValue(),
   });
 });
 
-routes.get("/api/average/energy/power", ( req, res ) => {
+routes.get("/api/average/energy/power", (req, res) => {
   return res.json({
     date: null,
     generatedPower: generateValue(),
@@ -17,72 +17,69 @@ routes.get("/api/average/energy/power", ( req, res ) => {
   });
 });
 
-routes.get('/api/daily/energy/performance', ( req, res ) => {
-  return res.json([{"date":"2016-12-21","performance":96.62},{"date":"2016-12-22","performance":96.48},{"date":"2016-12-23","performance":96.75},{"date":"2016-12-24","performance":96.48},{"date":"2016-12-25","performance":97.19},{"date":"2016-12-26","performance":97.44},{"date":"2016-12-27","performance":97.11},{"date":"2016-12-28","performance":98.22},{"date":"2016-12-29","performance":98.85},{"date":"2016-12-30","performance":96.97}]);
+routes.get("/api/daily/energy/performance", (req, res) => {
+  return res.json([
+    { date: "2022-05-02", performance: 101.75234417929792 },
+    { date: "2022-05-03", performance: generateValue(200) },
+    { date: "2022-05-04", performance: 97.18606988359983 },
+    { date: "2022-05-05", performance: generateValue(200) },
+    { date: "2022-05-06", performance: null },
+    { date: "2022-05-07", performance: null },
+  ]);
 });
 
-routes.get('/api/daily/energy/power', ( req, res ) => {
+routes.get("/api/daily/energy/power", (req, res) => {
   return res.json([
     {
-      date: "2016-12-21",
-      generatedPower: 25.37,
-      availablePower: 25.37,
+      date: "2022-05-02",
+      generatedPower: 10.080416666666668,
+      availablePower: generateValue(50),
     },
     {
-      date: "2016-12-22",
-      generatedPower: 25.41,
-      availablePower: 25.41,
+      date: "2022-05-03",
+      generatedPower: generateValue(50),
+      availablePower: 40.343707514805644,
     },
     {
-      date: "2016-12-23",
-      generatedPower: 24.08,
-      availablePower: 24.08,
+      date: "2022-05-04",
+      generatedPower: 16.17125,
+      availablePower: 16.639473146067512,
     },
     {
-      date: "2016-12-24",
-      generatedPower: 22.57,
-      availablePower: 22.57,
+      date: "2022-05-05",
+      generatedPower: generateValue(50),
+      availablePower: null,
     },
     {
-      date: "2016-12-25",
-      generatedPower: 16.17,
-      availablePower: 16.17,
+      date: "2022-05-06",
+      generatedPower: 12.76157894736842,
+      availablePower: generateValue(50),
     },
     {
-      date: "2016-12-26",
-      generatedPower: 13.85,
-      availablePower: 13.85,
-    },
-    {
-      date: "2016-12-27",
-      generatedPower: 12.79,
-      availablePower: 12.79,
-    },
-    {
-      date: "2016-12-28",
-      generatedPower: 3,
-      availablePower: 6.44,
-    },
-    {
-      date: "2016-12-29",
-      generatedPower: 14.84,
-      availablePower: 14.84,
-    },
-    {
-      date: "2016-12-30",
-      generatedPower: 12.18,
-      availablePower: 12.18,
+      date: "2022-05-07",
+      generatedPower: 2.435263157894737,
+      availablePower: null,
     },
   ]);
 });
 
-routes.get('/api/daily/inflow', ( req, res ) => {
-
-  return res.json([{"date":"2022-04-28","inflow":53},{"date":"2022-04-29","inflow":68},{"date":"2022-04-30","inflow":127},{"date":"2022-05-01","inflow":49},{"date":"2022-05-02","inflow":41},{"date":"2022-05-03","inflow":34},{"date":"2022-05-04","inflow":34},{"date":"2022-05-05","inflow":52},{"date":"2022-05-06","inflow":37},{"date":"2022-05-07","inflow":42}]);
+routes.get("/api/daily/inflow", (req, res) => {
+  return res.json([
+    { date: "2022-05-02", inflow: 198.389962546885 },
+    { date: "2022-05-03", inflow: generateValue(2000) },
+    { date: "2022-05-04", inflow: 1.73634680263062 },
+    { date: "2022-05-05", inflow: null },
+    { date: "2022-05-06", inflow: generateValue(2000) },
+    { date: "2022-05-07", inflow: null },
+  ]);
 });
 
-const generateValue = () => {
-  return Math.floor( Math.random() * ( 10 * 100 - 1 * 100 ) + 1 * 100 ) / ( 1 * 100 ) * 10;
+const generateValue = (limit = 100) => {
+  return (
+    (Math.floor(Math.random() * (10 * limit - 1 * limit) + 1 * limit) /
+      (1 * limit)) *
+    10
+  );
 };
 
 module.exports = routes;
